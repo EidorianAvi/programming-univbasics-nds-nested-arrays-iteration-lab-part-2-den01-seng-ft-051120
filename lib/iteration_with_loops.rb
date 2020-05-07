f42e7ab0152433e7d99b6b85ad26e9443e1c8806
Rene Avila <reneavila1993@gmail.com>
2020-05-07 20:01:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,3 +15,21 @@
 #   end
 #   new_array
 # end 
+
+
+def find_min_in_nested_arrays(src)
+  new_array = Arrau.new
+  row_index = 0 
+  while row_index < src.length do 
+    element_index = 0 
+    max = 1000
+    while element_index < src[row_index].length do 
+      if src[row_index][element_index] < max
+        max = src[row_index][element_index]
+        new_array << max
+      element_index += 1 
+    end
+    row_index += 1 
+  end
+  new_array
+end
